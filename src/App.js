497c5a87d1e688180b24516d438504e4b858afc9
tothricsaj <tothricsaj@gmail.com>
2020-03-25 20:57:05 +0100

    handling input change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {binary: 0}
+    this.handleInput = this.handleInput.bind(this)
+  }
+
+  handleInput(event) {
+    this.setState({binary: event.target.value})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form>
+          <input type="text" onChange={this.handleInput} />
+        </form>
+
+        <p>{this.state.binary}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
